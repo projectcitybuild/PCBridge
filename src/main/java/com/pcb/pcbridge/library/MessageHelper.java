de18e5fe44ceec894377714b4e9685493cf94b3b
Andy Saw <andy-saw@hotmail.com>
2015-10-24 17:19:36 +1100

    Created MessageHelper class to help with message format consistency

---
+++ src/main/java/com/pcb/pcbridge/library/MessageHelper.java
@@ -0,0 +1,51 @@
+package com.pcb.pcbridge.library;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Collection of message related helpers
+ */
+
+public final class MessageHelper 
+{
+	/**
+	 * Sends a message to a CommandSender based on the MessageType's colour and prefix
+	 * 
+	 * @param type
+	 * @param sender
+	 * @param message
+	 */
+	public static void Send(MessageType type, CommandSender sender, String message)
+	{
+		StringBuilder msg = new StringBuilder();
+		
+		switch(type)
+		{
+			case ERROR:
+				msg.append(ChatColor.RED).append("ERROR: ");
+				break;
+			case FATAL:
+				msg.append(ChatColor.RED).append(ChatColor.BOLD).append("FATAL: ");
+				break;
+			case INFO:
+				msg.append(ChatColor.GRAY).append("INFO: ");
+				break;
+			case CONFIRM:
+				msg.append(ChatColor.GREEN).append("CONFIRM: ");
+				break;
+			case WARNING:
+				msg.append(ChatColor.YELLOW).append("WARNING: ");
+				break;
+			default:
+				break;
+		}
+		
+		sender.sendMessage( msg.append(message).toString() );
+	}
+	
+	public static void Send(CommandSender sender, String message)
+	{
+		Send(MessageType.NORMAL, sender, message);
+	}
+}
---
+++ src/main/java/com/pcb/pcbridge/library/MessageType.java
@@ -0,0 +1,21 @@
+package com.pcb.pcbridge.library;
+
+public enum MessageType 
+{
+	NORMAL,
+	
+	// extra information to supply to the user
+	INFO,
+	
+	// something bad happened, execution cancelled (eg. bad command input formatting, invalid arguement, etc)
+	ERROR,
+	
+	// something bad happened, but continue anyway
+	WARNING,	
+	
+	// an exception was raised, cannot continue at all
+	FATAL,
+	
+	// prompt the user for further input
+	CONFIRM
+}
