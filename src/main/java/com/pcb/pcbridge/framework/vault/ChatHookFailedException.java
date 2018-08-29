dba05ebf8b57d0a3337825f6e18839f6808537ed
Andy <andy-saw@hotmail.com>
2018-08-30 00:40:42 +0900

    Remove files left over from merge

--- src/main/java/com/pcb/pcbridge/framework/vault/ChatHookFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.pcb.pcbridge.framework.vault;
-
-public class ChatHookFailedException extends Exception {
-    public ChatHookFailedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/pcb/pcbridge/framework/vault/PermissionHookFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.pcb.pcbridge.framework.vault;
-
-public class PermissionHookFailedException extends Exception {
-    public PermissionHookFailedException(String message) {
-        super(message);
-    }
-}
