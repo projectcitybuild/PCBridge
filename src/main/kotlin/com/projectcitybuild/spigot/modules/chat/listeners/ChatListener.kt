220d3e9abb40b1bdf32b2ba590711d179d92c3b9
Andy <andy-saw@hotmail.com>
2018-09-24 22:41:26 +0900

    Add chat formatting

--- src/main/kotlin/com/projectcitybuild/spigot/modules/chat/listeners/ChatListener.kt
+++ src/main/kotlin/com/projectcitybuild/spigot/modules/chat/listeners/ChatListener.kt
@@ -13,12 +13,38 @@ class ChatListener : Listenable<AsyncPlayerChatEvent> {
     override fun observe(event: AsyncPlayerChatEvent) {
         val environment = environment ?: return
 
+        // player muting
         val sendingPlayer = environment.get(event.player.uniqueId)
         if (sendingPlayer?.isMuted == true) {
             event.isCancelled = true
             event.player.sendMessage("You cannot chat while muted")
             return
         }
+
+        // chat formatting
+        val chat = environment.chat
+        if (chat == null) {
+            throw Exception("Failed to get chat hook")
+        }
+
+        val prefix = mutableListOf<String>(chat.getPlayerPrefix(event.player))
+        val suffix = mutableListOf<String>(chat.getPlayerSuffix(event.player))
+
+        environment.permissions?.getPlayerGroups(event.player)?.forEach { group ->
+            val groupPrefix = chat.getGroupPrefix(event.player.world, group)
+            val groupSuffix = chat.getGroupSuffix(event.player.world, group)
+
+            // donators have the [$] appear before everything
+            if (group.toLowerCase() == "donator") {
+                prefix.add(index = 0, element = groupPrefix)
+            } else {
+                prefix.add(groupPrefix)
+            }
+            suffix.add(groupSuffix)
+        }
+
+        val name = "${prefix.joinToString()} ${event.player.displayName} ${suffix.joinToString()}"
+        event.format = "<$name> ${event.message}"
     }
 
 }
