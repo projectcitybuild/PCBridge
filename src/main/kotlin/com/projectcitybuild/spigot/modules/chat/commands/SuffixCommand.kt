3c6d6adbfe336d674fdc1eaa845e49a94ca40d49
andyksaw <andy-saw@hotmail.com>
2019-03-02 23:13:33 +0900

    Update suffix command

--- src/main/kotlin/com/projectcitybuild/spigot/modules/chat/commands/SuffixCommand.kt
+++ src/main/kotlin/com/projectcitybuild/spigot/modules/chat/commands/SuffixCommand.kt
@@ -4,6 +4,7 @@ import com.projectcitybuild.spigot.extensions.getOnlinePlayer
 import com.projectcitybuild.core.contracts.Commandable
 import com.projectcitybuild.core.contracts.EnvironmentProvider
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class SuffixCommand : Commandable {
     override val label: String = "suffix"
@@ -13,7 +14,7 @@ class SuffixCommand : Commandable {
 
     override fun execute(sender: CommandSender, args: Array<String>, isConsole: Boolean): Boolean {
         if (environment == null) throw Exception("EnvironmentProvider missing")
-        if (args.isEmpty() || args.size > 2) return false
+        if (args.size < 1 || args.size > 3) return false
 
         val targetPlayerName = args.first()
         val targetPlayer = sender.server?.getOnlinePlayer(name = targetPlayerName)
@@ -27,16 +28,58 @@ class SuffixCommand : Commandable {
         }
 
         if (args.size == 1) {
-            environment?.chat?.setPlayerSuffix(targetPlayer, null)
-            sender.sendMessage("${targetPlayer.name} suffix reset")
+            getSuffixFor(player = targetPlayer, sender = sender)
             return true
         }
-        if (args.size == 2) {
-            val suffix = args[1]
-            environment?.chat?.setPlayerSuffix(targetPlayer, suffix)
-            sender.sendMessage("${targetPlayer.name} suffix set to $suffix")
+
+        when (args[1]) {
+            "set" -> {
+                if (args.size != 3) {
+                    return false
+                }
+                val newSuffix = args[2]
+                setSuffixFor(player = targetPlayer, sender = sender, newSuffix = newSuffix)
+            }
+            "clear" -> {
+                clearSuffixFor(player = targetPlayer, sender = sender)
+            }
+            else -> return false
         }
 
         return true
+    }
+
+    private fun setSuffixFor(player: Player, sender: CommandSender, newSuffix: String) {
+        val playerConfig = environment?.get(player = player.uniqueId)
+        if (playerConfig == null) {
+            return
+        }
+        playerConfig.suffix = newSuffix
+        sender.sendMessage("Updated suffix for ${player.name}")
+    }
+
+    private fun getSuffixFor(player: Player, sender: CommandSender) {
+        val playerConfig = environment?.get(player = player.uniqueId)
+        if (playerConfig == null) {
+            return
+        }
+        if (playerConfig.prefix == null) {
+            sender.sendMessage("${player.name} does not have a suffix set")
+        } else {
+            sender.sendMessage("Suffix for ${player.name}: ${playerConfig.suffix}")
+        }
+    }
+
+    private fun clearSuffixFor(player: Player, sender: CommandSender) {
+        val playerConfig = environment?.get(player = player.uniqueId)
+        if (playerConfig == null) {
+            return
+        }
+        if (playerConfig.suffix == null) {
+            sender.sendMessage("${player.displayName} does not have a suffix set")
+            return
+        }
+        playerConfig.suffix = null
+        sender.sendMessage("Updated suffix for ${player.displayName}")
     }
 }
