ede89df7aa2ef09c6ca8a482671857dea6a64268
Andy <andy-saw@hotmail.com>
2019-08-12 00:47:00 +0900

    Clean up MaintenanceCommand

--- src/main/kotlin/com/projectcitybuild/spigot/modules/maintenance/commands/MaintenanceCommand.kt
+++ src/main/kotlin/com/projectcitybuild/spigot/modules/maintenance/commands/MaintenanceCommand.kt
@@ -1,21 +1,25 @@
 package com.projectcitybuild.spigot.modules.maintenance.commands
 
 import com.projectcitybuild.core.contracts.Commandable
-import com.projectcitybuild.core.contracts.EnvironmentProvider
+import com.projectcitybuild.core.contracts.Environment
 import com.projectcitybuild.entities.models.PluginConfig
 import org.bukkit.command.CommandSender
 
 class MaintenanceCommand : Commandable {
-    override var environment: EnvironmentProvider? = null
+
+    enum class MaintenanceMode {
+        ON, OFF
+    }
+
+    override var environment: Environment? = null
     override val label: String = "maintenance"
-    override val permission: String = "pcbridge.maintenance"
 
     override fun execute(sender: CommandSender, args: Array<String>, isConsole: Boolean): Boolean {
-        val environment = environment ?: throw Exception("EnvironmentProvider missing")
+        val environment = environment ?: throw Exception("Environment missing")
         if (args.size > 1) return false
 
         val isMaintenanceMode = environment.get(PluginConfig.Settings.MAINTENANCE_MODE()) as? Boolean
-            ?: throw Exception("Cannot cast MAINTENANCE_MODE value to Boolean")
+                ?: throw Exception("Cannot cast MAINTENANCE_MODE value to Boolean")
 
         if (args.isEmpty()) {
             if (isMaintenanceMode) {
@@ -26,26 +30,31 @@ class MaintenanceCommand : Commandable {
             return true
         }
 
-        val newValue = args.first().toLowerCase()
-        if (newValue != "on" && newValue != "off") {
+        val newValueInput = args.first().toLowerCase()
+        if (!MaintenanceMode.values().map { it.name }.contains(newValueInput)) {
             return false
         }
 
-        if (newValue == "on") {
-            if (isMaintenanceMode) {
-                sender.sendMessage("Server is already in maintenance mode")
-            } else {
-                environment.set(PluginConfig.Settings.MAINTENANCE_MODE(), true)
-                sender.sendMessage("Server is now in maintenance mode")
-            }
-        } else {
-            if (!isMaintenanceMode) {
-                sender.sendMessage("Server is not in maintenance mode")
-            } else {
-                environment.set(PluginConfig.Settings.MAINTENANCE_MODE(), false)
-                sender.sendMessage("Server is now open to all players")
-            }
+        val newValue = MaintenanceMode.valueOf(newValueInput)
+
+        when (newValue) {
+            MaintenanceMode.ON ->
+                if (isMaintenanceMode) {
+                    sender.sendMessage("Server is already in maintenance mode")
+                } else {
+                    environment.set(PluginConfig.Settings.MAINTENANCE_MODE(), true)
+                    sender.sendMessage("Server is now in maintenance mode")
+                }
+
+            MaintenanceMode.OFF ->
+                if (!isMaintenanceMode) {
+                    sender.sendMessage("Server is not in maintenance mode")
+                } else {
+                    environment.set(PluginConfig.Settings.MAINTENANCE_MODE(), false)
+                    sender.sendMessage("Server is now open to all players")
+                }
         }
+
         return true
     }
 }
