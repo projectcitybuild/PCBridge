87015352d43f2967b62b7c902e521449486f7fc1
Mannriah <manning.telfer@me.com>
2019-09-22 23:42:46 +1000

    Replaced &9 with §9

--- src/main/kotlin/com/projectcitybuild/spigot/modules/ranks/commands/SyncCommand.kt
+++ src/main/kotlin/com/projectcitybuild/spigot/modules/ranks/commands/SyncCommand.kt
@@ -65,7 +65,7 @@ class SyncCommand : Commandable {
                     sender.sendMessage("Server failed to generate verification URL. Please try again later")
                     return@sync
                 }
-                sender.sendMessage("To link your account, please click the link and login if required:&9 ${json.data.url}")
+                sender.sendMessage("To link your account, please click the link and login if required:§9 ${json.data.url}")
             }
         }
 
