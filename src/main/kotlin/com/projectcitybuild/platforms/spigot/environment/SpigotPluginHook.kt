307740e2ace49a83e0fad222fb2677e77f8889e6
andyksaw <andy-saw@hotmail.com>
2021-07-19 21:58:10 +0900

    Fix trying to load LuckPerms before it loads

--- src/main/kotlin/com/projectcitybuild/platforms/spigot/environment/SpigotPluginHook.kt
+++ src/main/kotlin/com/projectcitybuild/platforms/spigot/environment/SpigotPluginHook.kt
@@ -1,20 +1,26 @@
 package com.projectcitybuild.platforms.spigot.environment
 
 import net.luckperms.api.LuckPerms
+import net.luckperms.api.LuckPermsProvider
 import org.bukkit.Bukkit
 
 class SpigotPluginHook {
+
+    private var _permissions: LuckPerms? = null
     var permissions: LuckPerms? = null
-        private set
+        get() {
+            if (_permissions == null) {
+                _permissions = setupPermissionHook()
+            }
+            return _permissions
+        }
 
-    init {
-        setupPermissionHook()
-    }
-
-    private fun setupPermissionHook() {
+    private fun setupPermissionHook(): LuckPerms? {
         val provider = Bukkit.getServicesManager().getRegistration(LuckPerms::class.java)
         if (provider != null) {
-            permissions = provider.provider
+            return provider.provider
         }
+        println("ERROR: LuckPerms not found - searching...")
+        return LuckPermsProvider.get()
     }
 }
