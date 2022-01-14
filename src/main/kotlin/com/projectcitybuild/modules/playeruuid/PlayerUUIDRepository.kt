cebf7ac4a1b52f2529dd8030c4b5f1ad300ded32
andyksaw <andy-saw@hotmail.com>
2022-01-15 01:30:02 +0900

    Catch 204 errors from Mojang API

--- src/main/kotlin/com/projectcitybuild/modules/playeruuid/PlayerUUIDRepository.kt
+++ src/main/kotlin/com/projectcitybuild/modules/playeruuid/PlayerUUIDRepository.kt
@@ -1,6 +1,7 @@
 package com.projectcitybuild.modules.playeruuid
 
 import com.projectcitybuild.core.extensions.toDashFormattedUUID
+import com.projectcitybuild.modules.network.APIClient
 import net.md_5.bungee.api.ProxyServer
 import java.util.*
 
@@ -12,10 +13,19 @@ class PlayerUUIDRepository(
         val onlinePlayer = proxyServer.players
             .firstOrNull { it.name.lowercase() == playerName.lowercase() }
 
-        if (onlinePlayer == null) {
+        if (onlinePlayer != null) {
+            return onlinePlayer.uniqueId
+        }
+
+        try {
             val mojangPlayer = getMojangPlayerAction.get(playerName = playerName)
             return UUID.fromString(mojangPlayer.uuid.toDashFormattedUUID())
+        } catch (e: APIClient.HTTPError) {
+            // "Username not found" is returned to us as a 204 HTTP error
+            if (e.errorBody?.status == 204) {
+                return null
+            }
+            throw e
         }
-        return onlinePlayer.uniqueId
     }
 }
