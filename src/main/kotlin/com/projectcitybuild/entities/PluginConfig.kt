9fd6a7a81c9ff55f60d78959a925d23b6d44fa44
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2022-01-17 01:09:06 +0900

    Restore missing config keys (#61)

--- src/main/kotlin/com/projectcitybuild/entities/PluginConfig.kt
+++ src/main/kotlin/com/projectcitybuild/entities/PluginConfig.kt
@@ -42,5 +42,49 @@ sealed class PluginConfig {
             "donator",
             "legacy-donator",
         )
+
+        val GROUPS_APPEARANCE_ADMIN_DISPLAY_NAME
+            = "groups.appearance.admin.display_name" defaultTo "§4[Staff]"
+        val GROUPS_APPEARANCE_ADMIN_HOVER_NAME
+            = "groups.appearance.admin.hover_name" defaultTo "Administrator"
+
+        val GROUPS_APPEARANCE_SOP_DISPLAY_NAME
+            = "groups.appearance.sop.display_name" defaultTo "§c[Staff]"
+        val GROUPS_APPEARANCE_SOP_HOVER_NAME
+            = "groups.appearance.sop.hover_name" defaultTo "Senior Operator"
+
+        val GROUPS_APPEARANCE_OP_DISPLAY_NAME
+            = "groups.appearance.op.display_name" defaultTo "§6[Staff]"
+        val GROUPS_APPEARANCE_OP_HOVER_NAME
+            = "groups.appearance.op.hover_name" defaultTo "Operator"
+
+        val GROUPS_APPEARANCE_MODERATOR_DISPLAY_NAME
+            = "groups.appearance.moderator.display_name" defaultTo "§e[Staff]"
+        val GROUPS_APPEARANCE_MODERATOR_HOVER_NAME
+            = "groups.appearance.moderator.hover_name" defaultTo "Moderator"
+
+        val GROUPS_APPEARANCE_TRUSTEDPLUS_HOVER_NAME
+            = "groups.appearance.trusted+.hover_name" defaultTo "Trusted+"
+
+        val GROUPS_APPEARANCE_TRUSTED_HOVER_NAME
+            = "groups.appearance.trusted.hover_name" defaultTo "Trusted"
+
+        val GROUPS_APPEARANCE_DONOR_HOVER_NAME
+            = "groups.appearance.donator.hover_name" defaultTo "Donor"
+
+        val GROUPS_APPEARANCE_ARCHITECT_HOVER_NAME
+            = "groups.appearance.architect.hover_name" defaultTo "Architect"
+
+        val GROUPS_APPEARANCE_ENGINEER_HOVER_NAME
+            = "groups.appearance.engineer.hover_name" defaultTo "Engineer"
+
+        val GROUPS_APPEARANCE_PLANNER_HOVER_NAME
+            = "groups.appearance.planner.hover_name" defaultTo "Planner"
+
+        val GROUPS_APPEARANCE_BUILDER_HOVER_NAME
+            = "groups.appearance.builder.hover_name" defaultTo "Builder"
+
+        val GROUPS_APPEARANCE_INTERN_HOVER_NAME
+            = "groups.appearance.intern.hover_name" defaultTo "Intern"
     }
 }
