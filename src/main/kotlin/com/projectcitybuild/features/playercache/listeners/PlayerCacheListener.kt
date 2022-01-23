a5a484c87461ee4c4177b69ce5cdd1c9fd5e2e2f
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2022-01-24 04:18:28 +0900

    Send a welcome message upon joining (#79)
    * Show welcome message upon joining network
    
    * Send welcome message for first-time joins
    
    * Add logging

--- src/main/kotlin/com/projectcitybuild/features/playercache/listeners/PlayerCacheListener.kt
+++ src/main/kotlin/com/projectcitybuild/features/playercache/listeners/PlayerCacheListener.kt
@@ -1,17 +1,22 @@
 package com.projectcitybuild.features.playercache.listeners
 
 import com.projectcitybuild.core.BungeecordListener
+import com.projectcitybuild.features.joinmessage.events.FirstTimeJoinEvent
+import com.projectcitybuild.modules.logger.PlatformLogger
 import com.projectcitybuild.modules.playerconfig.PlayerConfigCache
 import com.projectcitybuild.modules.playerconfig.PlayerConfigRepository
 import net.md_5.bungee.api.event.PlayerDisconnectEvent
 import net.md_5.bungee.api.event.PostLoginEvent
+import net.md_5.bungee.api.plugin.Plugin
 import net.md_5.bungee.event.EventHandler
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 class PlayerCacheListener @Inject constructor(
+    private val plugin: Plugin,
     private val playerCache: PlayerConfigCache,
     private val playerConfigRepository: PlayerConfigRepository,
+    private val logger: PlatformLogger,
 ): BungeecordListener {
 
     @EventHandler
@@ -19,6 +24,11 @@ class PlayerCacheListener @Inject constructor(
         val uuid = event.player.uniqueId
 
         if (playerConfigRepository.get(uuid) == null) {
+            logger.debug("No player config found for $uuid. Generating new one...")
+
+            plugin.proxy.pluginManager.callEvent(
+                FirstTimeJoinEvent(event.player)
+            )
             playerConfigRepository.add(
                 uuid = uuid,
                 isMuted = false,
