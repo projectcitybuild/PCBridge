bd92e83e901337540d80a2299736b5096770a34d
andyksaw <andy-saw@hotmail.com>
2022-01-25 21:18:22 +0900

    Add tests for warp list

--- src/main/kotlin/com/projectcitybuild/features/warps/commands/WarpsCommand.kt
+++ src/main/kotlin/com/projectcitybuild/features/warps/commands/WarpsCommand.kt
@@ -3,6 +3,7 @@ package com.projectcitybuild.features.warps.commands
 import com.projectcitybuild.core.InvalidCommandArgumentsException
 import com.projectcitybuild.entities.PluginConfig
 import com.projectcitybuild.features.warps.repositories.WarpRepository
+import com.projectcitybuild.features.warps.usecases.WarpListUseCase
 import com.projectcitybuild.modules.config.PlatformConfig
 import com.projectcitybuild.modules.textcomponentbuilder.send
 import com.projectcitybuild.platforms.bungeecord.environment.BungeecordCommand
@@ -20,8 +21,7 @@ import kotlin.math.max
 import kotlin.math.min
 
 class WarpsCommand @Inject constructor(
-    private val warpRepository: WarpRepository,
-    private val config: PlatformConfig
+    private val warpListUseCase: WarpListUseCase,
 ): BungeecordCommand {
 
     override val label: String = "warps"
@@ -32,30 +32,22 @@ class WarpsCommand @Inject constructor(
         if (input.args.size > 1) {
             throw InvalidCommandArgumentsException()
         }
+        val page = input.args.firstOrNull()?.toInt() ?: 1
 
-        var page = input.args.firstOrNull()?.toInt() ?: 1
-        val warpsPerPage = config.get(PluginConfig.WARPS_PER_PAGE)
-        val availableWarps = warpRepository.all().map { it.name }
-        val totalWarpPages = ceil((availableWarps.size / warpsPerPage).toDouble()).toInt()
-
-        if (availableWarps.isEmpty()) {
+        val warpList = warpListUseCase.getList(page)
+        if (warpList == null) {
             input.sender.send().info("No warps available")
             return
         }
 
-        page = min(page, totalWarpPages)
-
-        val warpList = availableWarps
-            .sorted()
-            .chunked(warpsPerPage)[max(page - 1, 0)]
-
-        val clickableWarps = warpList.map { name ->
-            TextComponent(name).also {
-                it.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/warp $name")
-                it.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text("/warp $name"))
-                it.isUnderlined = true
+        val clickableWarps = warpList.warps
+            .map { name ->
+                TextComponent(name).also {
+                    it.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/warp $name")
+                    it.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text("/warp $name"))
+                    it.isUnderlined = true
+                }
             }
-        }
 
         val clickableWarpList = TextComponent()
         clickableWarps.forEachIndexed { index, tc ->
@@ -67,7 +59,7 @@ class WarpsCommand @Inject constructor(
 
         val tc = TextComponent()
             .add("Warps") { it.isBold = true }
-            .addIf(totalWarpPages > 1, " ($page/$totalWarpPages)") {
+            .addIf(warpList.totalPages > 1, " ($page/${warpList.totalPages})") {
                 it.color = ChatColor.GRAY
             }
             .add("\n---\n")
