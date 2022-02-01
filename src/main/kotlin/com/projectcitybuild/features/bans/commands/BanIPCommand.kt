0453b55b478eba63219d85f1f5ef17790b733366
andyksaw <andy-saw@hotmail.com>
2022-02-01 23:08:22 +0900

    Allow name to be specified as first argument

--- src/main/kotlin/com/projectcitybuild/features/bans/commands/BanIPCommand.kt
+++ src/main/kotlin/com/projectcitybuild/features/bans/commands/BanIPCommand.kt
@@ -8,30 +8,33 @@ import com.projectcitybuild.features.bans.usecases.banip.BanIPUseCase
 import com.projectcitybuild.modules.textcomponentbuilder.send
 import com.projectcitybuild.platforms.bungeecord.environment.BungeecordCommand
 import com.projectcitybuild.platforms.bungeecord.environment.BungeecordCommandInput
+import net.md_5.bungee.api.ProxyServer
 import javax.inject.Inject
 
 class BanIPCommand @Inject constructor(
+    private val proxyServer: ProxyServer,
     private val banIPUseCase: BanIPUseCase,
 ): BungeecordCommand {
 
     override val label = "banip"
     override val permission = "pcbridge.ban.banip"
-    override val usageHelp = "/banip <ip> [reason]"
+    override val usageHelp = "/banip <name|ip> [reason]"
 
     override suspend fun execute(input: BungeecordCommandInput) {
         if (input.args.isEmpty()) {
             throw InvalidCommandArgumentsException()
         }
 
-        val targetIP = input.args.first()
+        val targetIP = proxyServer.players
+            .firstOrNull { it.name.lowercase() == input.args.first().lowercase() }
+            ?.socketAddress?.toString()
+            ?: input.args.first()
+
         val reason = input.args.joinWithWhitespaces(1 until input.args.size)
         val bannerName = if (input.isConsoleSender) "CONSOLE" else input.sender.name
 
-        val result = banIPUseCase.banIP(
-            targetIP,
-            bannerName,
-            reason,
-        )
+        val result = banIPUseCase.banIP(targetIP, bannerName, reason)
+
         if (result is Failure) {
             input.sender.send().error(
                 when (result.reason) {
