4bf02a0aea019c3eaf92e1bbeb613540bd557b0e
andyksaw <andy-saw@hotmail.com>
2022-02-09 03:36:28 +0900

    Remove unused donor API

--- src/main/kotlin/com/projectcitybuild/entities/responses/DonationTier.kt
+++
@@ -1,8 +0,0 @@
-package com.projectcitybuild.entities.responses
-
-import com.google.gson.annotations.SerializedName
-
-data class DonationTier(
-        @SerializedName("donation_tier_id") val id: Int,
-        @SerializedName("name") val name: String
-)
