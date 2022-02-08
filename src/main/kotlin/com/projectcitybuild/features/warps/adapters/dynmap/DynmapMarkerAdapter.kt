71f8666bf5b2260065c2995287bf9ea5448eded6
andyksaw <andy-saw@hotmail.com>
2022-02-08 20:25:49 +0900

    Create dynmap marker service class

---
+++ src/main/kotlin/com/projectcitybuild/features/warps/adapters/dynmap/DynmapMarkerAdapter.kt
@@ -0,0 +1,67 @@
+package com.projectcitybuild.features.warps.adapters.dynmap
+
+import com.projectcitybuild.features.warps.repositories.WarpRepository
+import com.projectcitybuild.modules.logger.PlatformLogger
+import org.bukkit.plugin.Plugin
+import org.dynmap.DynmapAPI
+import javax.inject.Inject
+
+class DynmapMarkerAdapter @Inject constructor(
+    private val plugin: Plugin,
+    private val warpRepository: WarpRepository,
+    private val logger: PlatformLogger,
+) {
+    class DynmapNotFoundException: Exception("Dynmap plugin not found")
+    class DynmapMarkerIconNotFoundException: Exception()
+
+    private val markerSetName = "pcbridge"
+
+    private lateinit var dynmap: DynmapAPI
+
+    fun enable() {
+        val plugin = plugin.server.pluginManager.getPlugin("dynmap")
+        if (plugin == null) {
+            logger.warning("Cannot find dynmap pluigin")
+            throw DynmapNotFoundException()
+        }
+        if (plugin !is DynmapAPI) {
+            logger.fatal("Found dynmap plugin but cannot access dynmap-api")
+            throw DynmapNotFoundException()
+        }
+    }
+
+    private fun updateWarpMarkers() {
+        val markerAPI = dynmap.markerAPI
+
+        val warpMarkerSet = markerAPI.getMarkerSet(markerSetName)
+            ?: markerAPI.createMarkerSet(
+                markerSetName,
+                "warps",
+                null, // TODO: what is this?
+                false, // TODO: what is this?
+            )
+
+        warpMarkerSet.layerPriority = 1
+        warpMarkerSet.hideByDefault = false
+
+        warpMarkerSet.markers.forEach {
+            it.deleteMarker()
+        }
+
+        val icon = markerAPI.getMarkerIcon("portal")
+            ?: throw DynmapMarkerIconNotFoundException()
+
+        warpRepository.all().forEach { warp ->
+            warpMarkerSet.createMarker(
+                "warp.${warp.name}",
+                warp.name,
+                warp.location.worldName,
+                warp.location.x,
+                warp.location.y,
+                warp.location.z,
+                icon,
+                false, // TODO: what is this?
+            )
+        }
+    }
+}
