7cbe1c102247fbd670ef8694743c5f50f66f2a4f
andyksaw <andy-saw@hotmail.com>
2022-02-09 03:45:04 +0900

    Add comments

--- src/main/kotlin/com/projectcitybuild/modules/eventbroadcast/LocalEventBroadcaster.kt
+++ src/main/kotlin/com/projectcitybuild/modules/eventbroadcast/LocalEventBroadcaster.kt
@@ -5,10 +5,9 @@ import org.bukkit.event.Event
 interface LocalEventBroadcaster {
 
     /**
-     * Broadcasts an event to all listeners on the
-     * current server
+     * Broadcasts an event to all listeners on the current server
      *
-     * @param event the event to broadcast
+     * @param event The event to broadcast
      */
     fun emit(event: Event)
 }
