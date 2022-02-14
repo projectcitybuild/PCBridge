45857a79db93f0461e07d24e7306a10d5d0564a0
andyksaw <andy-saw@hotmail.com>
2022-02-14 19:47:57 +0900

    Implement file cache

--- src/main/kotlin/com/projectcitybuild/modules/sharedcache/adapters/FlatFileSharedCacheSet.kt
+++ src/main/kotlin/com/projectcitybuild/modules/sharedcache/adapters/FlatFileSharedCacheSet.kt
@@ -3,7 +3,12 @@ package com.projectcitybuild.modules.sharedcache.adapters
 import com.projectcitybuild.entities.PluginConfig
 import com.projectcitybuild.modules.config.PlatformConfig
 import com.projectcitybuild.modules.sharedcache.SharedCacheSet
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import java.io.File
+import java.io.FileWriter
+import java.nio.charset.Charset
 import javax.inject.Inject
 
 class FlatFileSharedCacheSet @Inject constructor(
@@ -17,6 +22,9 @@ class FlatFileSharedCacheSet @Inject constructor(
     private val fileRelativePath = config.get(PluginConfig.SHARED_CACHE_FILE_RELATIVE_PATH) + "/" + fileName
     private val file = baseFolder.resolve(fileRelativePath)
 
+    @Serializable
+    data class Data(val set: Set<String>)
+
     private fun createFileIfNeeded() {
         if (!file.exists()) {
             file.mkdirs()
@@ -24,39 +32,49 @@ class FlatFileSharedCacheSet @Inject constructor(
         }
     }
 
+    private fun write(writeOperation: (MutableSet<String>) -> Unit) {
+        createFileIfNeeded()
+
+        val data = Json.decodeFromString<Data>(file.readText())
+        val mutatedSet = data.set.toMutableSet().also(writeOperation)
+        val json = Json.encodeToJsonElement(Data(mutatedSet))
+
+        FileWriter(file, Charset.defaultCharset()).use {
+            it.write(json.toString())
+        }
+    }
+
+    private fun readSet(): Set<String> {
+        val fileContents = file.readText()
+        val data = Json.decodeFromString<Data>(fileContents)
+        return data.set
+    }
+
     override fun has(value: String): Boolean {
         if (!file.exists()) return false
 
-        TODO()
+        return readSet().contains(value)
     }
 
     override fun add(value: String) {
-        createFileIfNeeded()
-
-        TODO()
+        write { it.add(value) }
     }
 
     override fun add(values: List<String>) {
-        createFileIfNeeded()
-
-        TODO()
+        write { it.addAll(values) }
     }
 
     override fun remove(value: String) {
-        createFileIfNeeded()
-
-        TODO()
+        write { it.remove(value) }
     }
 
     override fun removeAll() {
-        createFileIfNeeded()
-
-        TODO()
+        write { it.clear() }
     }
 
     override fun all(): Set<String> {
         if (!file.exists()) return emptySet()
 
-        TODO()
+        return readSet()
     }
 }
