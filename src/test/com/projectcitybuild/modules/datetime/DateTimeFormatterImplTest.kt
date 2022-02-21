c31e168d5742842f0c25f83a820ee1f1864538f2
andyksaw <andy-saw@hotmail.com>
2022-02-22 01:59:40 +0900

    Add formatter test

--- src/test/com/projectcitybuild/modules/datetime/DateTimeFormatterImplTest.kt
+++ src/test/com/projectcitybuild/modules/datetime/DateTimeFormatterImplTest.kt
@@ -3,9 +3,11 @@ package com.projectcitybuild.modules.datetime
 import com.projectcitybuild.modules.datetime.formatter.DateTimeFormatterImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.time.Month
 import java.time.ZoneId
 import java.time.format.FormatStyle
-import java.util.Locale
+import java.util.*
 
 class DateTimeFormatterImplTest {
 
@@ -16,6 +18,18 @@ class DateTimeFormatterImplTest {
             ZoneId.of("UTC"),
         )
         val result = formatter.convert(1642961224, FormatStyle.MEDIUM)
+
+        assertEquals("Jan 23, 2022, 6:07:04 PM", result)
+    }
+
+    @Test
+    fun `convert formats LocalDateTime with correct locale and timezone`() {
+        val formatter = DateTimeFormatterImpl(
+            Locale.forLanguageTag("en-us"),
+            ZoneId.of("UTC"),
+        )
+        val localDateTime = LocalDateTime.of(2022, Month.JANUARY, 23, 18, 7, 4)
+        val result = formatter.convert(localDateTime, FormatStyle.MEDIUM)
 
         assertEquals("Jan 23, 2022, 6:07:04 PM", result)
     }
