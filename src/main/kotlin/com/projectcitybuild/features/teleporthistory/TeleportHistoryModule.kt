d26bd9747bcab57889be5bb1a6b057787ae9c103
andyksaw <andy-saw@hotmail.com>
2022-03-22 02:32:15 +0900

    Clean up teleport history listener

--- src/main/kotlin/com/projectcitybuild/features/teleporthistory/TeleportHistoryModule.kt
+++ src/main/kotlin/com/projectcitybuild/features/teleporthistory/TeleportHistoryModule.kt
@@ -3,29 +3,20 @@ package com.projectcitybuild.features.teleporthistory
 import com.projectcitybuild.core.SpigotListener
 import com.projectcitybuild.core.contracts.SpigotFeatureModule
 import com.projectcitybuild.features.teleporthistory.commands.BackCommand
-import com.projectcitybuild.features.teleporthistory.listeners.PlayerSummonListener
-import com.projectcitybuild.features.teleporthistory.listeners.PlayerTeleportListener
-import com.projectcitybuild.features.teleporthistory.listeners.PlayerWarpListener
+import com.projectcitybuild.features.teleporthistory.listeners.TeleportListener
 import com.projectcitybuild.platforms.spigot.environment.SpigotCommand
 import javax.inject.Inject
 
-class TeleportHistoryModule {
+class TeleportHistoryModule @Inject constructor(
+    backCommand: BackCommand,
+    teleportListener: TeleportListener,
+) : SpigotFeatureModule {
 
-    class Spigot @Inject constructor(
-        backCommand: BackCommand,
-        playerTeleportListener: PlayerTeleportListener,
-        playerSummonListener: PlayerSummonListener,
-        playerWarpListener: PlayerWarpListener,
-    ) : SpigotFeatureModule {
+    override val spigotCommands: Array<SpigotCommand> = arrayOf(
+        backCommand,
+    )
 
-        override val spigotCommands: Array<SpigotCommand> = arrayOf(
-            backCommand,
-        )
-
-        override val spigotListeners: Array<SpigotListener> = arrayOf(
-            playerTeleportListener,
-            playerSummonListener,
-            playerWarpListener,
-        )
-    }
+    override val spigotListeners: Array<SpigotListener> = arrayOf(
+        teleportListener,
+    )
 }
---
+++ src/main/kotlin/com/projectcitybuild/features/teleporthistory/listeners/TeleportListener.kt
@@ -0,0 +1,54 @@
+package com.projectcitybuild.features.teleporthistory.listeners
+
+import com.projectcitybuild.core.SpigotListener
+import com.projectcitybuild.entities.CrossServerLocation
+import com.projectcitybuild.features.teleporting.events.PlayerPreSummonEvent
+import com.projectcitybuild.features.teleporting.events.PlayerPreTeleportEvent
+import com.projectcitybuild.integrations.shared.crossteleport.events.PlayerPreLocationTeleportEvent
+import com.projectcitybuild.modules.config.ConfigKey
+import com.projectcitybuild.modules.config.PlatformConfig
+import com.projectcitybuild.repositories.LastKnownLocationRepositoy
+import org.bukkit.Location
+import org.bukkit.event.EventHandler
+import java.util.*
+import javax.inject.Inject
+
+class TeleportListener @Inject constructor(
+    private val lastKnownLocationRepository: LastKnownLocationRepositoy,
+    private val config: PlatformConfig,
+) : SpigotListener {
+
+    @EventHandler
+    fun onPreLocationTeleport(event: PlayerPreLocationTeleportEvent) {
+        rememberLastLocation(
+            playerUUID = event.player.uniqueId,
+            location = event.currentLocation,
+        )
+    }
+
+    @EventHandler
+    fun onPrePlayerSummon(event: PlayerPreSummonEvent) {
+        rememberLastLocation(
+            playerUUID = event.summonedPlayer.uniqueId,
+            location = event.currentLocation,
+        )
+    }
+
+    @EventHandler
+    fun onPrePlayerTeleport(event: PlayerPreTeleportEvent) {
+        rememberLastLocation(
+            playerUUID = event.player.uniqueId,
+            location = event.currentLocation,
+        )
+    }
+
+    private fun rememberLastLocation(playerUUID: UUID, location: Location) {
+        lastKnownLocationRepository.set(
+            playerUUID = playerUUID,
+            location = CrossServerLocation.fromLocation(
+                serverName = config.get(ConfigKey.SPIGOT_SERVER_NAME),
+                location = location,
+            )
+        )
+    }
+}
