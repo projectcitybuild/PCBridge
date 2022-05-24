610ea954c964ada5c6ed590265dc97eb1fe3809a
andyksaw <andy-saw@hotmail.com>
2022-05-25 01:13:36 +0900

    Add flush sub command

---
+++ src/main/kotlin/com/projectcitybuild/features/utilities/usecases/ReloadPluginUseCase.kt
@@ -0,0 +1,15 @@
+package com.projectcitybuild.features.utilities.usecases
+
+import com.projectcitybuild.features.chat.ChatGroupFormatter
+import com.projectcitybuild.modules.playercache.PlayerConfigCache
+import javax.inject.Inject
+
+class ReloadPluginUseCase @Inject constructor(
+    private val chatGroupFormatter: ChatGroupFormatter,
+    private val playerConfigCache: PlayerConfigCache,
+) {
+    fun execute() {
+        chatGroupFormatter.flushCache()
+        playerConfigCache.flush()
+    }
+}
