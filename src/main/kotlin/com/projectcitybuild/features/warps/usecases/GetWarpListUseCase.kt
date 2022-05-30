37876be68e93f47c2edac083cf393039336e7b64
andyksaw <andy-saw@hotmail.com>
2022-05-31 02:21:32 +0900

    Add total warp count to warp list

--- src/main/kotlin/com/projectcitybuild/features/warps/usecases/GetWarpListUseCase.kt
+++ src/main/kotlin/com/projectcitybuild/features/warps/usecases/GetWarpListUseCase.kt
@@ -13,6 +13,7 @@ class GetWarpListUseCase @Inject constructor(
     private val config: PlatformConfig
 ) {
     data class WarpList(
+        val totalWarps: Int,
         val totalPages: Int,
         val currentPage: Int,
         val warps: List<String>,
@@ -34,6 +35,7 @@ class GetWarpListUseCase @Inject constructor(
             .chunked(warpsPerPage)[max(currentPage - 1, 0)]
 
         return WarpList(
+            totalWarps = availableWarps.size,
             totalPages = totalWarpPages,
             currentPage = currentPage,
             warps = warpList,
