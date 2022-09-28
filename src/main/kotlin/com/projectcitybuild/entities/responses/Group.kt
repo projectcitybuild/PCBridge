38cff3fc7e543605f1cb33ed1449a975cad9b0c6
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2022-09-28 11:33:56 +0900

    Fix Group model not deserializing correctly (#177)

--- src/main/kotlin/com/projectcitybuild/entities/responses/Group.kt
+++ src/main/kotlin/com/projectcitybuild/entities/responses/Group.kt
@@ -7,16 +7,7 @@ data class Group(
     @SerializedName("name") val name: String = "name",
     @SerializedName("alias") val alias: String? = null,
     @SerializedName("minecraft_name") val minecraftName: String?,
-    @SerializedName("is_default") private val _isDefault: Int = 0,
-    @SerializedName("is_staff") private val _isStaff: Int = 0,
-    @SerializedName("is_admin") private val _isAdmin: Int = 0
-) {
-    val isDefaultRank: Boolean
-        get() = _isDefault == 1
-
-    val isStaffRank: Boolean
-        get() = _isStaff == 1
-
-    val isAdminRank: Boolean
-        get() = _isAdmin == 1
-}
+    @SerializedName("is_default") private val _isDefault: Boolean = false,
+    @SerializedName("is_staff") private val _isStaff: Boolean = false,
+    @SerializedName("is_admin") private val _isAdmin: Boolean = false
+)
