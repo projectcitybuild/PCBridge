b33d51da6af4c74c0824135dc959f0db95807d53
andyksaw <andy-saw@hotmail.com>
2023-07-09 19:12:57 +0900

    Fix null args

--- pcbridge-spigot/src/main/kotlin/com/projectcitybuild/commands/NightVisionCommand.kt
+++ pcbridge-spigot/src/main/kotlin/com/projectcitybuild/commands/NightVisionCommand.kt
@@ -11,30 +11,38 @@ import org.bukkit.potion.PotionEffectType
 class NightVisionCommand : SpigotCommand {
     override val label = "nv"
     override val permission = "pcbridge.build.nightvision"
-    override val usageHelp = "/nv [off|on]"
+    override val usageHelp = "/nv [on|off]"
 
     override suspend fun execute(input: SpigotCommandInput) {
         if (input.isConsole) {
             throw CannotInvokeFromConsoleException()
         }
-        if (input.args.size > 1 || !listOf("on", "off").contains(input.args.first())) {
+        if (input.args.size > 1) {
             throw InvalidCommandArgumentsException()
         }
-        val duration = -1
-        val amplifier = 0
+        if (input.args.isNotEmpty() && !listOf("on", "off").contains(input.args.firstOrNull())) {
+            throw InvalidCommandArgumentsException()
+        }
+
+        val duration = Integer.MAX_VALUE
+        val amplifier = 1
         val potionEffectType = PotionEffectType.NIGHT_VISION
         val potionEffect = PotionEffect(potionEffectType, duration, amplifier)
 
         val player = input.player
-        val alreadyHasEffect = player.hasPotionEffect(potionEffectType)
+        val toggleOn = if (input.args.isNotEmpty()) {
+            input.args.first() == "on"
+        } else {
+            !player.hasPotionEffect(potionEffectType)
+        }
 
         player.removePotionEffect(potionEffectType)
 
-        if (input.args.first() == "on" || !alreadyHasEffect) {
+        if (toggleOn) {
             player.addPotionEffect(potionEffect)
-            player.send().info("Nightvision toggled on. Type /nv to turn it off")
+            player.send().info("NightVision toggled on. Type /nv to turn it off")
         } else {
-            player.send().info("Nightvision toggled off")
+            player.send().info("NightVision toggled off")
         }
     }
 }
