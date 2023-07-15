fe0be72b84e0b193fdcdb6f3d61194750b41d327
andyksaw <andy-saw@hotmail.com>
2023-07-16 00:10:08 +0900

    Register badge command

--- pcbridge-spigot/src/main/kotlin/com/projectcitybuild/Permissions.kt
+++ pcbridge-spigot/src/main/kotlin/com/projectcitybuild/Permissions.kt
@@ -4,5 +4,6 @@ class Permissions private constructor() {
     companion object {
         const val COMMAND_BUILD_NIGHTVISION = "pcbridge.build.nightvision"
         const val COMMAND_BUILD_INVIS_FRAME = "pcbridge.build.invisframe"
+        const val COMMAND_CHAT_TOGGLE_BADGE = "pcbridge.chat.badge"
     }
 }
