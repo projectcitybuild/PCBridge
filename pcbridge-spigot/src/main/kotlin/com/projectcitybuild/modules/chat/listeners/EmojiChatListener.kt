2e4fa2560bb7de05439997c47abd519db5817b32
andyksaw <andy-saw@hotmail.com>
2023-08-20 18:20:21 +0900

    Add more emojis

--- pcbridge-spigot/src/main/kotlin/com/projectcitybuild/modules/chat/listeners/EmojiChatListener.kt
+++ pcbridge-spigot/src/main/kotlin/com/projectcitybuild/modules/chat/listeners/EmojiChatListener.kt
@@ -6,9 +6,25 @@ import org.bukkit.event.player.AsyncPlayerChatEvent
 
 class EmojiChatListener: SpigotListener<AsyncPlayerChatEvent> {
 
+    companion object {
+        val emojis: Map<String, String> = mapOf(
+            Pair(":skull:", "☠"),
+            Pair(":heart:", "❤"),
+            Pair(":fire:", "\uD83D\uDD25"),
+            Pair(":tm:", "™"),
+        )
+
+        val regex = emojis.keys
+            .joinToString(separator = "|")
+            .let { pattern -> "(?i)($pattern)" }  // Add case-insensitivity
+            .toRegex()
+    }
+
     @EventHandler
     override suspend fun handle(event: AsyncPlayerChatEvent) {
-        event.message = event.message
-            .replace(oldValue = ":skull:", newValue = "☠", ignoreCase = true)
+        event.message = event.message.replace(
+            regex = regex,
+            transform = { match -> emojis[match.value.lowercase()] ?: match.value },
+        )
     }
 }
