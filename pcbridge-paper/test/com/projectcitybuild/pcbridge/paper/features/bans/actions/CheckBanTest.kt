bcdf493a12bbb1d5ad3bfaecf9ca922efd9d9995
andyksaw <andy-saw@hotmail.com>
2025-02-24 15:50:24 +0900

    Fix tests

--- pcbridge-paper/test/com/projectcitybuild/pcbridge/paper/features/bans/actions/CheckBanTest.kt
+++ pcbridge-paper/test/com/projectcitybuild/pcbridge/paper/features/bans/actions/CheckBanTest.kt
@@ -5,6 +5,7 @@ import com.projectcitybuild.pcbridge.http.pcb.models.IPBan
 import com.projectcitybuild.pcbridge.http.pcb.models.PlayerBan
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -39,81 +40,55 @@ class CheckBanTest {
     fun `should deny if player is banned`() =
         runTest {
             val ban = playerBan()
-            val result =
-                useCase.authorize(
-                    PlayerData(playerBan = ban),
-                )
-            assertEquals(
-                CheckBan.ConnectResult.Denied(
-                    CheckBan.Ban.UUID(ban),
-                ),
-                result,
+            val result = useCase.get(
+                PlayerData(playerBan = ban),
             )
+            assertEquals(CheckBan.Ban.UUID(ban), result)
         }
 
     @Test
     fun `should not deny if player ban is inactive`() =
         runTest {
-            val ban =
-                playerBan().copy(
-                    unbannedAt = LocalDate
-                        .now()
-                        .atStartOfDay(ZoneId.systemDefault())
-                        .toLocalDateTime(),
-                )
-            val result =
-                useCase.authorize(
-                    PlayerData(playerBan = ban),
-                )
-            assertEquals(
-                CheckBan.ConnectResult.Allowed,
-                result,
+            val ban = playerBan().copy(
+                unbannedAt = LocalDate
+                    .now()
+                    .atStartOfDay(ZoneId.systemDefault())
+                    .toLocalDateTime(),
             )
+            val result = useCase.get(
+                PlayerData(playerBan = ban),
+            )
+            assertNull(result)
         }
 
     @Test
     fun `should deny if IP is banned`() =
         runTest {
             val ban = ipBan()
-            val result =
-                useCase.authorize(
-                    PlayerData(ipBan = ban),
-                )
-            assertEquals(
-                CheckBan.ConnectResult.Denied(
-                    CheckBan.Ban.IP(ban),
-                ),
-                result,
+            val result = useCase.get(
+                PlayerData(ipBan = ban),
             )
+            assertEquals(CheckBan.Ban.IP(ban), result)
         }
 
     @Test
     fun `should not deny if IP ban is inactive`() =
         runTest {
-            val ban =
-                ipBan().copy(
-                    unbannedAt = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toLocalDateTime(),
-                )
-            val result =
-                useCase.authorize(
-                    PlayerData(ipBan = ban),
-                )
-            assertEquals(
-                CheckBan.ConnectResult.Allowed,
-                result,
+            val ban = ipBan().copy(
+                unbannedAt = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toLocalDateTime(),
             )
+            val result = useCase.get(
+                PlayerData(ipBan = ban),
+            )
+            assertNull(result)
         }
 
     @Test
     fun `should allow if not banned`() =
         runTest {
-            val result =
-                useCase.authorize(
-                    PlayerData(playerBan = null, ipBan = null),
-                )
-            assertEquals(
-                CheckBan.ConnectResult.Allowed,
-                result,
+            val result = useCase.get(
+                PlayerData(playerBan = null, ipBan = null),
             )
+            assertNull(result)
         }
 }
