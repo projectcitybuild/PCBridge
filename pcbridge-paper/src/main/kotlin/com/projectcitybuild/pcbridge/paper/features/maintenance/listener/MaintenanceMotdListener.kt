b44ee6bef75a1eac003ad8d80a32a2e99e30658a
andyksaw <andy-saw@hotmail.com>
2025-05-02 17:34:45 +0900

    Prioritize maintenance motd

--- pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/maintenance/listener/MaintenanceMotdListener.kt
+++ pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/maintenance/listener/MaintenanceMotdListener.kt
@@ -4,12 +4,13 @@ import com.destroystokyo.paper.event.server.PaperServerListPingEvent
 import com.projectcitybuild.pcbridge.paper.architecture.state.Store
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 class MaintenanceMotdListener(
     private val store: Store,
 ) : Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun onServerListPing(event: PaperServerListPingEvent) {
         val state = store.state
         if (state.maintenance) {
