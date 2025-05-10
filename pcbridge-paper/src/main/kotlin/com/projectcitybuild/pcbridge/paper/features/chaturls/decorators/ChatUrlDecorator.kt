c6b574ce2283e54f65b1daf726439e6ed214e6a3
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2025-05-10 19:37:41 +0900

    refactor: replace legacy url extractor (#305)

--- pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/chaturls/decorators/ChatUrlDecorator.kt
+++ pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/chaturls/decorators/ChatUrlDecorator.kt
@@ -2,20 +2,31 @@ package com.projectcitybuild.pcbridge.paper.features.chaturls.decorators
 
 import com.projectcitybuild.pcbridge.paper.architecture.chat.decorators.ChatMessage
 import com.projectcitybuild.pcbridge.paper.architecture.chat.decorators.ChatMessageDecorator
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import net.kyori.adventure.text.TextReplacementConfig
+import net.kyori.adventure.text.event.ClickEvent
+import java.util.regex.Pattern
 
 class ChatUrlDecorator: ChatMessageDecorator {
-    override suspend fun decorate(prev: ChatMessage): ChatMessage {
-        // Only the legacy serializer automatically converts URLs to clickable text
-        val legacySerializer = LegacyComponentSerializer
-            .builder()
-            .extractUrls()
-            .build()
+    private val urlReplacer = TextReplacementConfig
+        .builder()
+        .match(DEFAULT_URL_PATTERN)
+        .replacement { component ->
+            val url = component.content().let { content ->
+                if (!URL_SCHEME_PATTERN.matcher(content).find()) "https://$content"
+                else content
+            }
+            component.clickEvent(ClickEvent.openUrl(url))
+        }
+        .build()
 
+    override suspend fun decorate(prev: ChatMessage): ChatMessage {
         return prev.copy(
-            message = legacySerializer.deserialize(
-                legacySerializer.serialize(prev.message)
-            )
+            message = prev.message.replaceText(urlReplacer),
         )
+    }
+
+    private companion object {
+        val DEFAULT_URL_PATTERN: Pattern = Pattern.compile("(?:(https?)://)?([-\\w_.]+\\.\\w{2,})(/\\S*)?")
+        val URL_SCHEME_PATTERN: Pattern = Pattern.compile("^[a-z][a-z0-9+\\-.]*:")
     }
 }
