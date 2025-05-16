fc152b8db689aff125558ecc8c7a4fb716b654f3
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2025-05-16 19:34:56 +0900

    feat: read emoji mapping from remote config (#312)

--- pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/chatemojis/ChatEmojisModule.kt
+++ pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/chatemojis/ChatEmojisModule.kt
@@ -1,10 +1,24 @@
 package com.projectcitybuild.pcbridge.paper.features.chatemojis
 
 import com.projectcitybuild.pcbridge.paper.features.chatemojis.decorators.ChatEmojiDecorator
+import com.projectcitybuild.pcbridge.paper.features.chatemojis.listeners.EmojiConfigListener
+import com.projectcitybuild.pcbridge.paper.features.chatemojis.repositories.EmojiRepository
 import org.koin.dsl.module
 
 val chatEmojisModule = module {
     factory {
-        ChatEmojiDecorator()
+        ChatEmojiDecorator(
+            emojiRepository = get(),
+        )
+    }
+
+    factory {
+        EmojiConfigListener(
+            emojiRepository = get(),
+        )
+    }
+
+    single {
+        EmojiRepository()
     }
 }
