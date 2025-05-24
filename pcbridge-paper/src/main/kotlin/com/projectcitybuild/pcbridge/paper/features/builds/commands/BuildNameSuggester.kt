5300477660f81accf1e6bbb0499959d9614c6304
andyksaw <andy-saw@hotmail.com>
2025-05-24 20:22:03 +0900

    Suggest on Dispatchers.IO

--- pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/builds/commands/BuildNameSuggester.kt
+++ pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/builds/commands/BuildNameSuggester.kt
@@ -4,6 +4,8 @@ import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import com.projectcitybuild.pcbridge.paper.features.builds.repositories.BuildRepository
 import io.papermc.paper.command.brigadier.CommandSourceStack
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class BuildNameSuggester(
     private val buildRepository: BuildRepository,
@@ -11,7 +13,7 @@ class BuildNameSuggester(
     suspend fun suggest(
         context: CommandContext<CommandSourceStack>,
         suggestions: SuggestionsBuilder,
-    ) {
+    ) = withContext(Dispatchers.IO) {
         val name = suggestions.remaining.lowercase()
 
         buildRepository.names()
