f9c35334612ccc4a3f1b9b503bd0b5d3c8c363d7
andyksaw <andy-saw@hotmail.com>
2025-05-30 17:39:03 +0900

    Add workstations

---
+++ pcbridge-paper/src/main/kotlin/com/projectcitybuild/pcbridge/paper/features/workstations/commands/SmithingTableCommand.kt
@@ -0,0 +1,31 @@
+package com.projectcitybuild.pcbridge.paper.features.workstations.commands
+
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.tree.LiteralCommandNode
+import com.projectcitybuild.pcbridge.paper.PermissionNode
+import com.projectcitybuild.pcbridge.paper.core.support.brigadier.BrigadierCommand
+import com.projectcitybuild.pcbridge.paper.core.support.brigadier.extensions.executesSuspending
+import com.projectcitybuild.pcbridge.paper.core.support.brigadier.extensions.requirePlayer
+import com.projectcitybuild.pcbridge.paper.core.support.brigadier.extensions.requiresPermission
+import com.projectcitybuild.pcbridge.paper.core.support.brigadier.traceSuspending
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.papermc.paper.command.brigadier.Commands
+import org.bukkit.plugin.Plugin
+
+class SmithingTableCommand(
+    private val plugin: Plugin,
+) : BrigadierCommand {
+    override fun buildLiteral(): LiteralCommandNode<CommandSourceStack>
+        = Commands.literal("smithingtable")
+            .requiresPermission(PermissionNode.WORKSTATIONS_USE)
+            .executesSuspending(plugin, ::execute)
+            .build()
+
+    private suspend fun execute(context: CommandContext<CommandSourceStack>) = context.traceSuspending {
+        val player = context.source.requirePlayer()
+
+        val attachLocation = null // No table to attach to
+        val force = true // Don't check for a table at the attach location
+        player.openSmithingTable(attachLocation, force)
+    }
+}
